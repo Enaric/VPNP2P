5683841d6b7363859f75790863b80a5d9d1d68c6
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-02-02 21:39:13 +0800

    Node start

---
+++ InitTest/node_info.c
@@ -0,0 +1,386 @@
+#include "node_info.h"
+
+int startsWith(const char *pre, const char *str) {
+    size_t lenpre = strlen(pre),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? 0 : memcmp(pre, str, lenpre) == 0;
+}
+
+char *strip(const char *s) {
+    char *p = malloc(strlen(s) + 1);
+    if(p) {
+        char *p2 = p;
+        while(*s != '\0') {
+            if(*s != '\t' && *s != '\n') {
+                *p2++ = *s++;
+            } else {
+                ++s;
+            }
+        }
+        *p2 = '\0';
+    }
+    return p;
+}
+
+int struct2file(struct Node *node, char *filename) {
+    FILE *fp;
+    if ((fp = fopen(filename, "w")) == NULL) {
+        printf("Open file failed.\n");
+        return -1;
+    }
+    // 遍历node列表，写入
+    struct Node *p;
+    struct IP *ip;
+    for(p = node; p != NULL;p = p->next_node) {
+        // 标记开始
+        fprintf(fp, "NODE\n");
+       
+        // 正式写入
+        fprintf(fp, "id: %d\n", p->id);
+        fprintf(fp, "node_count: %d\n", p->node_count);
+        fprintf(fp, "reliable: %d\n", p->reliable);
+        fprintf(fp, "ip_list\n");
+        // 遍历ip列表
+        for (ip = p->ip_list; ip != NULL; ip = ip->next_ip) {
+            fprintf(fp, "ip: %s\n", ip->ip);
+            fprintf(fp, "ip_type: %s\n", ip->type);
+            fprintf(fp, "s_count: %d\n", ip->s_count);
+        }
+        fprintf(fp, "ip_list_ends\n");
+        fprintf(fp, "end_node\n");
+    }
+   
+
+    fclose(fp);
+
+    return 0;
+}
+
+struct Node* file2struct(char *filename) {
+    FILE *fp;
+    char *strline;
+    char buffer[255];
+    // todo 不用 node_tail
+    struct Node *node_new = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *header = (struct Node*)malloc(sizeof(struct Node));
+
+    header->id = 0;
+    header->next_node = node_new;
+    struct Node *prev = header;
+
+    struct IP *ip_new = (struct IP*)malloc(sizeof(struct IP));
+    struct IP *ip_header = (struct IP*)malloc(sizeof(struct IP));
+    fp = fopen(filename, "r+");
+    if (fp == NULL) {
+        printf("is NULL\n");
+    }
+    while (fgets(buffer, 255, (FILE*) fp)) {
+        strline = &buffer[0];
+        strline[strlen(strline) - 1] = 0;
+        // printf("strline: %s\n", strline);
+        if (startsWith("NODE", strline)) {
+            // 是一个Node的开始
+            // node_new = (struct Node*)malloc(sizeof(struct Node));
+            // node_new->next_node = NULL;
+        } else if (startsWith("id: ", strline)) {
+            // Node的id
+            //printf("line is: %s, len is: %d\n", strline, strlen(strline));
+            strline = strline + 4;
+            node_new->id = atoi(strline);
+            //printf("id is: %d\n", node_new->id);
+        } else if (startsWith("node_count: ", strline)) {
+            strline = strline + 12;
+            node_new->node_count = atoi(strline);
+        } else if (startsWith("reliable: ", strline)) {
+            strline = strline + 10;
+            node_new->reliable = atoi(strline);
+        } else if (startsWith("ip_list_ends", strline)) {
+            ip_header->next_ip = NULL;
+        } else if (startsWith("ip: ", strline)) {
+            strline = strline + 4;
+            char *ip = strip(strline);
+            strcpy(ip_new->ip, ip);
+        } else if (startsWith("ip_type: ", strline)) {
+            strline += 9;
+            char *ip_type = strip(strline);
+            //printf("this is ip_type: %s\n", ip_type);
+            strcpy(ip_new->type, ip_type);
+        } else if (startsWith("s_count: ", strline)) {
+            strline += 9;
+            ip_new->s_count = atoi(strline);
+            // printf("this is s_count: %d\n", atoi(strline));
+            // 创建一个新的ip struct
+            ip_header = ip_header->next_ip;
+            struct IP *ip_tmp = (struct IP*)malloc(sizeof(struct IP));
+            ip_tmp->next_ip = NULL;
+            ip_new->next_ip = ip_tmp;
+            ip_new = ip_new->next_ip;
+        } else if (startsWith("ip_list", strline)) {
+            // ip列表的开始
+            ip_new = (struct IP*)malloc(sizeof(struct IP));
+            ip_header = (struct IP*)malloc(sizeof(struct IP));
+            ip_header->next_ip = ip_new;
+            ip_new->next_ip = NULL;
+            node_new->ip_list = ip_new;
+        } else if (startsWith("end_node", strline)) {
+            struct Node *node_next = (struct Node*)malloc(sizeof(struct Node));
+            node_new->next_node = node_next;
+            node_new = node_new->next_node;
+            prev = prev->next_node;
+        }
+    }
+    prev->next_node = NULL;
+    return header->next_node;
+}
+
+// 比较两个node是否完全相同
+int node_is_identical(struct Node *node1, struct Node *node2) {
+    struct IP *ip1 = node1->ip_list;
+    struct IP *ip2 = node2->ip_list;
+
+    if (node1->id != node2->id) {
+        return 0;
+    }
+    
+    if (node1->node_count != node2->node_count) {
+        return 0;
+    }
+
+    while (1)
+    {
+        if (ip1 == NULL && ip2 == NULL) {
+            return 1;
+        }
+        if ((ip1 == NULL && ip2 != NULL) || (ip1 != NULL && ip2 == NULL)) {
+            return 0;
+        }
+        if ((!strcmp(ip1->ip, ip2->ip)) || (!strcmp(ip1->type, ip2->type)) || (ip1->s_count != ip2->s_count)) {
+            return 0;
+        }
+        ip1 = ip1->next_ip;
+        ip2 = ip2->next_ip;
+    }
+    
+}
+
+int ip_list_merge(struct IP *ip1, struct IP *ip2) {
+    struct IP *ip_list_tail = NULL;
+    for (ip_list_tail = ip1;ip_list_tail->next_ip != NULL;ip_list_tail = ip_list_tail->next_ip) {
+        // 遍历到最后一个IP
+    }
+
+    struct IP *ip_ptr = NULL;
+    for (ip_ptr = ip2;ip_ptr != NULL; ip_ptr = ip_ptr->next_ip) {
+        struct IP *ip_tmp = NULL;
+        int flag = 1;
+        for (ip_tmp = ip1;ip_tmp != NULL;ip_tmp = ip_tmp->next_ip) {
+            if (strcmp(ip_tmp->ip, ip_ptr->ip) == 0) {
+                // 相同ip，根据s_count合并
+                flag = 0;
+                if(ip_ptr->s_count > ip_tmp->s_count) {
+                    strcpy(ip_tmp->ip, ip_ptr->ip);
+                    strcpy(ip_tmp->type, ip_ptr->type);
+                    ip_tmp->s_count = ip_ptr->s_count;
+                } else if (ip_ptr->s_count == ip_tmp->s_count) {
+                    if ((strcmp(ip_ptr->ip, ip_tmp->ip) != 0) || (strcmp(ip_ptr->type, ip_tmp->type) != 0)) {
+                        // 强冲突
+                        return -1;
+                    }
+                }
+            }
+        }
+        // 不存在的ip，直接接到链表尾部
+        if (flag) {
+            ip_list_tail->next_ip = ip_ptr;
+            ip_list_tail = ip_list_tail->next_ip;
+        }
+    }
+
+    return 1;
+}
+
+// 集群信息合并，结果统一放在第一个node中
+// 如果发生强冲突 返回 -1
+int node_merge(struct Node* node1, struct Node* node2) {
+    // printf("node1: \n");
+    // print_node(node1);
+    // printf("node2: \n");
+    // print_node(node2);
+    struct Node *ptr1;
+    struct Node *ptr2;
+    struct Node *remain = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *p = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *prev;
+    prev = remain;
+    remain->next_node = p;
+    
+    for (ptr2 = node2; ptr2 != NULL; ptr2 = ptr2->next_node) {
+        int flag = 1;
+        for (ptr1 = node1; ptr1 != NULL; ptr1 = ptr1->next_node) {
+            if (ptr1->id == ptr2->id) {
+                flag = 0;
+                if (ptr1->node_count < ptr2->node_count) {
+                    ptr1->node_count = ptr2->node_count;
+                    ptr1->reliable = ptr2->reliable;
+                    ptr1->ip_list = ptr2->ip_list;
+                }
+                // todo node_count相等
+            }
+        }
+        if (flag) {
+            p->id = ptr2->id;
+            p->node_count = ptr2->node_count;
+            p->reliable = ptr2->reliable;
+            p->ip_list = ptr2->ip_list;
+            struct Node *p_next = (struct Node*)malloc(sizeof(struct Node));
+            p->next_node = p_next;
+            p = p->next_node;
+            prev = prev->next_node;
+        }
+    }
+    for (ptr1 = node1; ptr1->next_node != NULL; ptr1 = ptr1->next_node) {
+
+    }
+    prev->next_node = NULL;
+    ptr1->next_node = remain->next_node;
+}
+
+int contain_node(struct Node *list, struct Node *node) {
+    struct Node *tmp;
+    for (tmp = list; tmp != NULL; tmp = tmp->next_node) {
+        if (tmp->id == node->id) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void print_node(struct Node *node) {
+    struct Node *ptr = NULL;
+    struct IP *ip = NULL;
+    for (ptr = node;ptr != NULL;ptr = ptr->next_node) {
+        printf("node id: %d, node count: %d\n", ptr->id, ptr->node_count);
+        printf("node ip list: \n");
+        for (ip = ptr->ip_list;ip != NULL;ip = ip->next_ip) {
+            printf("ip: %s, type: %s, s_count: %d\n", ip->ip, ip->type, ip->s_count);
+        }
+    }
+}
+
+struct Node* convert_buf_to_node(char *buf) {
+    char dst[50][80];
+    int cnt = split(dst, buf, "\n");
+
+    struct Node *node_new = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *header = (struct Node*)malloc(sizeof(struct Node));
+
+    header->id = 0;
+    header->next_node = node_new;
+    struct Node *prev = header;
+
+    struct IP *ip_new = NULL;
+    struct IP *ip_header = NULL;
+
+    for (int i = 0;i < cnt;i++) {
+        if (startsWith("NODE", dst[i])) {
+            // 是一个Node的开始
+        } else if (startsWith("id:", dst[i])) {
+            char id[10];
+            memcpy(id, dst[i] + 4, 10);
+            node_new->id = atoi(id);
+        } else if (startsWith("node_count:", dst[i])) {
+            char node_count[10];
+            memcpy(node_count, dst[i] + 12, 10);
+            node_new->node_count = atoi(node_count);
+        } else if (startsWith("reliable:", dst[i])) {
+            char reliable[10];
+            memcpy(reliable, dst[i] + 10, 10);
+            node_new->reliable = atoi(reliable);
+        } else if (startsWith("ip_list_ends", dst[i])) {
+            ip_header->next_ip = NULL;
+        } else if (startsWith("ip:", dst[i])) {
+            char ip[20];
+            memcpy(ip, dst[i] + 4, 20);
+            strcpy(ip_new->ip, ip);
+        } else if (startsWith("ip_type:", dst[i])) {
+            char ip_type[10];
+            memcpy(ip_type, dst[i] + 9, 10);
+            strcpy(ip_new->type, ip_type);
+        } else if (startsWith("s_count:", dst[i])) {
+            char s_count[10];
+            memcpy(s_count, dst[i] + 9, 10);
+            ip_new->s_count = atoi(s_count);
+
+            ip_header = ip_header->next_ip;
+            struct IP *ip_tmp = (struct IP*)malloc(sizeof(struct IP));
+            ip_tmp->next_ip = NULL;
+            ip_new->next_ip = ip_tmp;
+            ip_new = ip_tmp;
+        } else if (startsWith("ip_list", dst[i])) {
+            // ip列表的开始
+            ip_new = (struct IP*)malloc(sizeof(struct IP));
+            ip_header = (struct IP*)malloc(sizeof(struct IP));
+            ip_header->next_ip = ip_new;
+            ip_new->next_ip = NULL;
+            node_new->ip_list = ip_new;
+        } else if (startsWith("end_node", dst[i])) {
+            struct Node *node_next = (struct Node*)malloc(sizeof(struct Node));
+            node_new->next_node = node_next;
+            node_new = node_new->next_node;
+            prev = prev->next_node;
+        }
+    }
+    prev->next_node = NULL;
+    return header->next_node;
+}
+
+int convert_node_to_buf(struct Node *node, char buf[2048]) {
+     // 遍历node列表，写入
+    struct Node *p;
+    struct IP *ip;
+    int offset = 0;
+    // static char buf[2048];
+    for(p = node; p != NULL;p = p->next_node) {
+        // 标记开始
+        offset += sprintf(buf + offset, "NODE\n");
+        
+        // 正式写入
+        offset += sprintf(buf + offset, "id: %d\n", p->id);
+        offset += sprintf(buf + offset, "node_count: %d\n", p->node_count);
+        offset += sprintf(buf + offset, "reliable: %d\n", p->reliable);
+        offset += sprintf(buf + offset, "ip_list\n");
+        // 遍历ip列表
+        for (ip = p->ip_list; ip != NULL; ip = ip->next_ip) {
+            // printf("ip: %s\n", ip->ip);
+            // printf("ip_type: %s\n", ip->type);
+            // printf("s_count: %d\n", ip->s_count);
+            offset += sprintf(buf + offset, "ip: %s\n", ip->ip);
+            offset += sprintf(buf + offset, "ip_type: %s\n", ip->type);
+            offset += sprintf(buf + offset, "s_count: %d\n", ip->s_count);
+        }
+        offset += sprintf(buf + offset, "ip_list_ends\n");
+        offset += sprintf(buf + offset, "end_node\n");
+    }
+    return offset;
+}
+
+int split(char dst[][80], char *str, const char *spl) {
+    int n = 0;
+    char *result = NULL;
+    result = strtok(str, spl);
+    while( result != NULL )
+    {
+        strcpy(dst[n++], result);
+        result = strtok(NULL, spl);
+    }
+    return n;
+}
+
+// int main() {
+//     struct Node *node1 = file2struct("node_info.txt");
+//     struct Node *node2 = file2struct("test_info.txt");
+//     //print_node(node1);
+//     node_merge(node1, node2);
+//     print_node(node1);
+// }
--- InitTest/node_start.c
+++ InitTest/node_start.c
@@ -6,46 +6,130 @@
 #include <stdio.h>
 #include <fcntl.h> // for open
 #include <unistd.h> // for close
+#include "../Util/util.h"
+#include "../Util/message.h"
+#include "node_info.h"
 
-struct IP {
-    char ip[20];    //ip
-    char type[20];        //unknownIP/clientIP/serverIP
-    int s_count;
-    struct IP *next_ip;
-};
+int client_socket_fd;
+struct sockaddr_in client_addr;
+struct sockaddr_in server_addr;
+// int Listen_PORT;
+struct Node *local_node;             // 本地节点
 
-struct Node {//node
-    int id;
-    int node_count;
-    struct IP *ipList;
-    struct Node *next_node;
-};
-
-int get_local_ip(char *ip){
-    int fd, intrface, retn = 0;
-    struct ifreq buf[INET_ADDRSTRLEN];
-    struct ifconf ifc;
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) >= 0){
-        ifc.ifc_len = sizeof(buf);
-        // caddr_t,Linux内核源码里定义的：typedef void *caddr_t；
-        ifc.ifc_buf = (caddr_t)buf;
-        if (!ioctl(fd, SIOCGIFCONF, (char *)&ifc)){
-            intrface = ifc.ifc_len/sizeof(struct ifreq);
-            while (intrface-- > 0){
-                if (!(ioctl(fd, SIOCGIFADDR, (char *)&buf[intrface]))){
-                    ip=(inet_ntoa(((struct sockaddr_in*)(&buf[intrface].ifr_addr))->sin_addr));
-                    printf("IP:%s\n", ip);
-                }
-            }
-        }
-        close(fd);
+int free_program() {
+    if(-1 != client_socket_fd){
+        close(client_socket_fd);
+        client_socket_fd = -1;
     }
     return 0;
 }
 
-int main(){
-    char ip[64];
-    memset(ip, 0, sizeof(ip));
-    get_local_ip(ip);
+int init_socket() {
+    bzero(&client_addr, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    client_addr.sin_port = htons(0); 
+
+    client_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket_fd < 0) {
+        perror("Create socket failed");
+        exit(1);
+    }
+}
+
+int generate_info() {
+    // 标定内网ip
+    // 生成一个全局的节点信息数据结构
+    // 可以提供给后面的流程使用
+    struct Node tmp_node;
+    bzero(&tmp_node, sizeof(tmp_node));
+    tmp_node.id = generate_id(); // todo 全局唯一id生成器
+    tmp_node.node_count = 1;
+    tmp_node.reliable = RELIABLE;
+    struct IP *ip = (struct IP *)malloc(sizeof(struct IP));
+    tmp_node.ip_list = ip;
+    char **local_ip_list = get_local_ip();
+    for(int i = 0;i < IP_LIST_SIZE;i++) {
+        if (local_ip_list[i] == NULL) {
+            break;
+        }
+        strcpy(ip->ip, local_ip_list[i]);
+        if (is_intranet(local_ip_list[i])) {
+            strcpy(ip->type, "clientIP");
+        } else {
+            strcpy(ip->type, "unknownIP");
+        }
+        ip->s_count = 0;
+
+        if (local_ip_list[i+1] != NULL) {
+            struct IP ip_new;
+            bzero(&ip_new, sizeof(ip_new));
+            ip->next_ip = &ip_new;
+            ip = &ip_new;
+        }
+    }
+    // 将生成的节点信息写到文件
+    struct2file(&tmp_node, "InitTest/node_info.txt");
+
+    if (ip != NULL) { free(ip); ip = NULL; }
+    if (local_ip_list != NULL) { free(local_ip_list); local_ip_list = NULL; }
+    return 0;
+}
+
+// 遍历local_node 的ip列表，确认每个local ip都存在
+// 如果缓存中ip和本地ip对应，返回true，否则返回false
+int judge_local_ip() {
+    struct IP *ip;
+    char **node_ip_list = malloc(IP_LIST_SIZE * sizeof(char*));
+    int pos = 0;
+    for (ip = local_node->ip_list;ip != NULL;ip = ip->next_ip) {
+        node_ip_list[pos] = ip->ip;
+        pos++;
+    }
+
+    if (ip_match(node_ip_list, pos)) {
+        // 沿用之前的infofile
+        // todo 更新打洞信息
+        return 1;
+    } 
+    
+    if (node_ip_list != NULL) { free(node_ip_list); node_ip_list = NULL; } 
+    return 0;
+}
+
+// 初始搭建，第一个Node必须指定一个serverIP
+//./net start serverIP1 [, serverIP2, infofile=xxx.json]
+// server_ip 使用的自身的server ip(可以多个，可以设计成list)
+// use_cache 可选参数，是否读取本地的集群信息
+int start(char *server_ip, int use_cache) {
+    init_socket();
+
+    if (!fopen("InitTest/node_info.txt", "r")) {
+        // 不存在infofile
+        // 生成本地infofile
+        printf("infofile not exist\n");
+        generate_info();
+    } else {
+        printf("infofile exist\n");
+    }
+    
+    if (use_cache) {
+        // 校对本地集群缓存信息
+        if(!judge_local_ip()) {
+            // 更新 node count
+            local_node->node_count += 1;
+            free(local_node->ip_list);
+            local_node->ip_list = NULL;
+            free(local_node->next_node);
+            local_node->next_node = NULL;
+
+            struct2file(local_node, "InitTest/node_info.txt");
+        }
+    }
+    return 0;
+}
+
+int main(int argc,char *argv[]) {
+    start(argv[1], 0);
     return 0;
 }
